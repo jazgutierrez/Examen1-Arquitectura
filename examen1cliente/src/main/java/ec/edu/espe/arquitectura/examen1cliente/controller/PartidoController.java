a94a639ff4f20bd986aa2f5451f7be3da2e461be
Jazz <yazbel2009@hotmail.com>
2021-11-29 22:10:17 -0500

    listar controller

--- examen1cliente/src/main/java/ec/edu/espe/arquitectura/examen1cliente/controller/PartidoController.java
+++ examen1cliente/src/main/java/ec/edu/espe/arquitectura/examen1cliente/controller/PartidoController.java
@@ -5,12 +5,15 @@
 package ec.edu.espe.arquitectura.examen1cliente.controller;
 
 import ec.edu.espe.arquitectura.examen1cliente.service.PartidoService;
+import ec.edu.espe.arquitectura.examen1cliente.wsdl.ListarLocalidadesDisponiblesRequest;
+import ec.edu.espe.arquitectura.examen1cliente.wsdl.ListarLocalidadesResponse;
 import ec.edu.espe.arquitectura.examen1cliente.wsdl.ObtenerPartidosDisponiblesRequest;
 import ec.edu.espe.arquitectura.examen1cliente.wsdl.ObtenerPartidosDisponiblesResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
@@ -26,6 +29,19 @@ public class PartidoController {
         ObtenerPartidosDisponiblesRequest request = new ObtenerPartidosDisponiblesRequest();
         ObtenerPartidosDisponiblesResponse response=this.service.listarPartidosDisponibles(request);
         if(response.getPartido().size()>0){
+        return ResponseEntity.ok(response);     
+        }
+        else{
+            return ResponseEntity.badRequest().build();
+        }
+    }
+    
+    @GetMapping(value = "localidad/{codigo}")
+    public ResponseEntity listarLocalidadDisponible(@PathVariable("codigo") String codigo){
+        ListarLocalidadesDisponiblesRequest request = new ListarLocalidadesDisponiblesRequest();
+        request.setCodigo(codigo);
+        ListarLocalidadesResponse response=this.service.listarLocalidadesDisponibles(request);
+        if(response.getLocalidad().size()>0){
         return ResponseEntity.ok(response);     
         }
         else{
